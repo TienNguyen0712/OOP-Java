034bab9f4de4960f9288793fd6886fdf6a936e4e
Nguyễn Đăng Tiến <131792899+TienNguyen0712@users.noreply.github.com>
2025-09-18 22:08:17 +0700

    upload Bai2.jaav

---
+++ Bai2.java
@@ -0,0 +1,12 @@
+public class Bai2 {
+    // Phương thức Method được khởi tạo tròng hàm chương trình và được gọi trong hàm main
+    static void Method() {
+        // Code 
+    } // Cú pháp khởi tạo phương thức
+
+    //Parameter: là tham số dược truyền vào phương thức ( Xem như là biến trong phương thức ) khi viết phương thức số lượng parameter 
+    // khi truyền vào là vô kể và ngăn cách bằng dấu phẩy, void thì không dùng return còn lại thì thao kiểu dữ liệu 
+    // Một phương thức có thể có nhiều tên giống nhau tuy nhiên phải khác kiểu dữ liệu 
+    // Một biến được gán sau khi nó được sinh ra biến chỉ được sử dụng trong một khối cũng có thể là trong vòng lặp, một lớp
+    // Đệ quy trong Java được sử dụng rỗng rãi
+}
